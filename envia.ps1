423bb1ed6d8f60c0a4e156c526467ddb10547ede
Fernando Della Torre <fernando.dellatorre@bs4it.com.br>
2021-11-06 17:04:43 -0300

    teste 04

--- envia.ps1
+++ envia.ps1
@@ -1,5 +1,6 @@
 $json = Get-Content -Path ./teste.json
-$url = "http://localhost:81/insert2.php"
+$url = "http://localhost:81/webservice/get_gaps.php"
 
 $web = Invoke-WebRequest -SkipCertificateCheck -Uri $url -Method Post -Body $json -ContentType 'application/json; charset=UTF-8' 
 
+$web
